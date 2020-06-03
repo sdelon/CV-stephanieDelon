72964c9b3f8a0610994d2091b38467b2a74eb350
sdelon <53929569+sdelon@users.noreply.github.com>
2020-06-03 16:11:09 +0200

    updating resume
    updating resume

--- .eleventy.js
+++ .eleventy.js
@@ -3,6 +3,7 @@ const md = new MarkdownIt();
 
 module.exports = function(eleventyConfig) {
     eleventyConfig.addFilter('markdown', value => md.renderInline(value));
+    eleventyConfig.addFilter('isArr', item => Array.isArray(item));
 
     return {
         dir: {
